178005b6ec78de6f838c2695eb92d6e4f91cbffc
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-15 13:46:26 +0900

    chore: heroku build

--- src/api/products.js
+++ src/api/products.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//TODO : 예외처리 해주기
 export const getProducts = async () => {
   const response = await axios.get('http://localhost:4000/products');
   return response.data;
