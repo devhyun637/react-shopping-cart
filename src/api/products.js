7b8ce401f2ac5b7a928c24c76d72e9f1774fe2b0
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-15 18:04:24 +0900

    fix: change json-server url

--- src/api/products.js
+++ src/api/products.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 //TODO : 예외처리 해주기
 export const getProducts = async () => {
-  const response = await axios.get('https://shopping-cart-tyhe.herokuapp.com/api');
+  const response = await axios.get('https://shopping-cart-tyhe.herokuapp.com/api/products');
   return response.data;
 };
