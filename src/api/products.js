08d70c462059082079fff9e3affb60d49c9981b0
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-15 17:59:44 +0900

    chore: change json-sever deploy url

--- src/api/products.js
+++ src/api/products.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 //TODO : 예외처리 해주기
 export const getProducts = async () => {
-  const response = await axios.get('http://localhost:4000/products');
+  const response = await axios.get('https://shopping-cart-tyhe.herokuapp.com/api');
   return response.data;
 };
