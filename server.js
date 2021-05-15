0cad295bc5c0641ca18750f8d8192ecde310d85e
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-15 14:13:53 +0900

    chore: build

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('./src/data/products.json');
 const middlewares = jsonServer.defaults({
   static: './build',
 });
